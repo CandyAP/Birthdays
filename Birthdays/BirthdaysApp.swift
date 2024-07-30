56975e3175806782f171632f42c5a9b173d2daa9
candyap <candyap182@icloud.com>
2024-07-30 23:20:57 +0700

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Candy Pangestu on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirthdaysTests/BirthdaysTests.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysTests.swift
+//  BirthdaysTests
+//
+//  Created by Candy Pangestu on 7/30/24.
+//
+
+import Testing
+@testable import Birthdays
+
+struct BirthdaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
